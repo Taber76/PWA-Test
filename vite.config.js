698cff1cab90abb7faf0b8c905bfe734b921b138
Tabare Bermudez <tabarebermudez@gmail.com>
2024-04-22 12:52:56 -0300

    fix: :bug: Vite config

--- vite.config.js
+++ vite.config.js
@@ -31,12 +31,6 @@ const manifest = {
         sizes: "512x512",
         type: "image/png",
       },
-      {
-        src: "/warehouse_192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-        purpose: "any maskable",
-      },
     ],
   },
 };
